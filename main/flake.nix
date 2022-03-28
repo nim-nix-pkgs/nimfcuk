df4e1546bdcf660b41dda6a3280eebc6c5298ed6
hugosenari <hugosenari@gmail.com>
2022-03-27 21:53:13 -0300

    chore: re index nimfcuk

--- main/flake.nix
+++ main/flake.nix
@@ -11,7 +11,6 @@
   inputs.src-nimfcuk-main.ref   = "refs/heads/main";
   inputs.src-nimfcuk-main.owner = "2KAbhishek";
   inputs.src-nimfcuk-main.repo  = "nimfcuk";
-  inputs.src-nimfcuk-main.dir   = "";
   inputs.src-nimfcuk-main.type  = "github";
   
   inputs."docopt".owner = "nim-nix-pkgs";
